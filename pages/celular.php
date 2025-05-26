5539d6c8cc5ac3c3d93ed89603379078bca6a524
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-05-26 16:10:59 -0300

    🔧 (celular.php): altera o arquivo CSS de celular.css para global.css
    A mudança do arquivo CSS de celular.css para global.css visa unificar o estilo visual da aplicação, promovendo a reutilização de estilos e facilitando a manutenção do código. Isso garante que as alterações de estilo sejam consistentes em diferentes partes do site, reduzindo a duplicação de código CSS.

--- pages/celular.php
+++ pages/celular.php
@@ -7,7 +7,7 @@
     <meta name="author" content="Marcelo de Araujo Almeida">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/celular.css">
+    <link rel="stylesheet" href="../css/global.css">
     <link rel="stylesheet" href="../css/menu-drop.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../scripts/menudrop.js" defer></script>
