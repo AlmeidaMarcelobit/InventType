6c878478d64154e488bf220c3e62d327b0395502
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-05-26 16:10:26 -0300

    ✨ (novos-colaboradore.php): adiciona nova página para exibir informações de novos colaboradores
    Cria uma nova página HTML para exibir detalhes de novos colaboradores, como nome, CPF, superior imediato, data de início prevista, e-mail, usuário AD, empresa e centro de custo. Esta adição visa melhorar a gestão e visualização de informações de novos colaboradores, facilitando o acesso e a organização dos dados relevantes para o processo de integração.

---
+++ pages/novos-colaboradore.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Novos Colaboradores</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+
+</head>
+
+<body id="corpo">
+<?php include '../includes/header.php'; ?>
+<div id="conainer">
+    <div id="card">
+        <h1>Ticket: #141110</h1>
+        <div class="info"><span class="label">Candidato:</span> Beatriz Lima Gouvêa</div>
+        <div class="info"><span class="label">CPF:</span>526.497.578-76 </div>
+        <div class="info"><span class="label">Fabíola Todesco Quiles:</span>Superior Imediato </div>
+        <div class="info"><span class="label">Data prevista de Início:</span>02/06/2025</div>
+        <div class="info"><span class="label">E-mail:</span>beatriz.gouvea@amorsaude.com</div>
+        <div class="info"><span class="label">User AD:</span> beatrizgouvea</div>
+        <div class="info"><span class="label">Empresa:</span>DentalVidas</div>
+        <div class="info"><span class="label">Centro de Custo:</span>71001 - Administrativo</div>
+        <div class="info"><span class="label">Numero Kits:</span></div>
+    </div>
+
+</div>
+
+<footer>
+    <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
+</footer>
+</body></html>
