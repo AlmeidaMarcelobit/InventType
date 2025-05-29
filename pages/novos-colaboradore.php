1cdaf4f3f56dee4b5b5862ac447e5bc010e0381c
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-05-29 12:21:18 -0300

    ✨📝 (novos-colaboradore.php): atualiza a página com novos colaboradores e corrige estrutura
    Expande a lista de colaboradores na página, adicionando novos
    registros e informações detalhadas para cada um. Isso melhora a
    visibilidade e a organização dos dados dos colaboradores. Além disso,
    corrige a estrutura HTML, garantindo que a página termine com as
    tags adequadas, melhorando a conformidade com padrões HTML.

--- pages/novos-colaboradore.php
+++ pages/novos-colaboradore.php
@@ -19,16 +19,118 @@
 <?php include '../includes/header.php'; ?>
 <div class="container">
     <div class="card">
-        <div class="info"><span class="label">Ticket:</span> #141110</div>
-        <div class="info"><span class="label">Candidato:</span> Beatriz Lima Gouvêa</div>
-        <div class="info"><span class="label">CPF:</span>526.497.578-76 </div>
-        <div class="info"><span class="label">Fabíola Todesco Quiles:</span>Superior Imediato </div>
-        <div class="info"><span class="label">Data prevista de Início:</span>02/06/2025</div>
-        <div class="info"><span class="label">E-mail:</span>beatriz.gouvea@amorsaude.com</div>
-        <div class="info"><span class="label">User AD:</span> beatrizgouvea</div>
-        <div class="info"><span class="label">Empresa:</span>DentalVidas</div>
-        <div class="info"><span class="label">Centro de Custo:</span>71001 - Administrativo</div>
-        <div class="info"><span class="label">Numero Kits:</span></div>
+        <div class="info">Ticket:<span class="label"></span></div>
+        <div class="info">Candidato:<span class="label"></span></div>
+        <div class="info">CPF:<span class="label"></span></div>
+        <div class="info">Superior Imediato:<span class="label"></span></div>
+        <div class="info">Cargo de Admissão:<span class="label"></span></div>
+        <div class="info">Substituição:<span class="label"></span></div>
+        <div class="info">E-mail:<span class="label"></span></div>
+        <div class="info">User AD:<span class="label"></span></div>
+        <div class="info">Empresa:<span class="label"></span></div>
+        <div class="info">Centro de Custo:<span class="label"></span></div>
+        <div class="info">Numero Kits:<span class="label"></span></div>
+    </div>
+    <div class="card">
+        <div class="info">Colaborador:<span class="label">Pedro Luiz Vicente de Carvalho</span></div>
+        <div class="info">Substituição:<span class="label">Lucilene Santos Matias</span></div>
+        <div class="info">Cargo:<span class="label">Consultor de Performance</span></div>
+        <div class="info">Departamento:<span class="label">Consultoria</span></div>
+        <div class="info">E-mail:<span class="label">pedro.carvalho@amorsaude.com</span></div>
+        <div class="info">CPF:<span class="label">470.311.428-30</span></div>
+        <div class="info">Centro de Custo:<span class="label">12010 - Consultoria de Performance</span></div>
+    </div>
+    <div class="card">
+        <div class="info">Colaborador:<span class="label">Julia Carolina Rogerio</span></div>
+        <div class="info">Substituição:<span class="label">Yago dos Anjos Mariano</span></div>
+        <div class="info">Departamento:<span class="label">Consultoria de Performance</span></div>
+        <div class="info">E-mail:<span class="label"></span></div>
+        <div class="info">CPF:<span class="label">377.938.578-36</span></div>
+        <div class="info">Centro de Custo:<span class="label">12010 - Consultoria de Performance</span></div>
+    </div>
+    <div class="card">
+        <div class="info">Colaborador:<span class="label">Agata Cristina Silva Assunção</span></div>
+        <div class="info">Cargo:<span class="label">Supervisor de Operações</span></div>
+        <div class="info">Departamento:<span class="label">Backoffice e SAF</span></div>
+        <div class="info">E-mail:<span class="label">agata.assuncao@amorsaude.com</span></div>
+        <div class="info">CPF:<span class="label">376.102.618-82</span></div>
+        <div class="info">Centro de Custo:<span class="label">12005 - BackOffice</span></div>
+    </div>
+    <div class="card">
+        <div class="info">Colaborador:<span class="label">Brenno Yan Romanatti Rogério</span></div>
+        <div class="info">Substituição:<span class="label">Ana Beatriz Silveira</span></div>
+        <div class="info">Cargo:<span class="label">Analista de QA JR</span></div>
+        <div class="info">Departamento:<span class="label">Produto</span></div>
+        <div class="info">E-mail:<span class="label">brenno.rogerio@amorsaude.com</span></div>
+        <div class="info">CPF:<span class="label">>456.510.218-84</span></div>
+        <div class="info">Centro de Custo:<span class="label">13013 - Produto</span></div>
+    </div>
+    <div class="card">
+        <div class="info">Ticket:<span class="label"></span></div>
+        <div class="info">Candidato:<span class="label">Thauany Heloisa Pereira</span></div>
+        <div class="info">CPF:<span class="label">414.972.498-99</span></div>
+        <div class="info">Superior Imediato:<span class="label"></span></div>
+        <div class="info">Cargo de Admissão:<span class="label">Analista de Administração Pessoal JR</span></div>
+        <div class="info">Departamento:<span class="label">Pessoas e Cultura</span></div>
+        <div class="info">Substituição:<span class="label"></span></div>
+        <div class="info">E-mail:<span class="label">thauany.pereira@amorsaude.com</span></div>
+        <div class="info">User AD:<span class="label"></span></div>
+        <div class="info">Empresa:<span class="label"></span></div>
+        <div class="info">Centro de Custo:<span class="label">13007 - Remuneração e Benefícios</span></div>
+        <div class="info">Numero Kits:<span class="label"></span></div>
+    </div>
+    <div class="card">
+        <div class="info">Ticket:<span class="label"></span></div>
+        <div class="info">Candidato:<span class="label">Marina Pereira Pardinho Ramos</span></div>
+        <div class="info">CPF:<span class="label">323.808.848-38</span></div>
+        <div class="info">Superior Imediato:<span class="label"></span></div>
+        <div class="info">Departamento:<span class="label">Gestão de Rede</span></div>
+        <div class="info">Cargo de Admissão:<span class="label">Analista de Documentações Gestão de Rede SR</span></div>
+        <div class="info">Substituição:<span class="label"></span></div>
+        <div class="info">E-mail:<span class="label">marina.ramos@amorsaude.com</span></div>
+        <div class="info">User AD:<span class="label"></span></div>
+        <div class="info">Empresa:<span class="label"></span></div>
+        <div class="info">Centro de Custo:<span class="label">12006 - Gestão de Rede</span></div>
+        <div class="info">Numero Kits:<span class="label"></span></div>
+    </div>
+    <div class="card">
+        <div class="info">Ticket:<span class="label">#141110</span></div>
+        <div class="info">Candidato:<span class="label">Beatriz Lima Gouvêa</span></div>
+        <div class="info">CPF:<span class="label">526.497.578-76</span></div>
+        <div class="info">Superior Imediato:<span class="label">Fabíola Todesco Quiles</span></div>
+        <div class="info">Data prevista de Início:<span class="label">02/06/2025</span></div>
+        <div class="info">E-mail:<span class="label">beatriz.gouvea@amorsaude.com</span></div>
+        <div class="info">User AD:<span class="label">beatrizgouvea</span></div>
+        <div class="info">Empresa:<span class="label">DentalVidas</span></div>
+        <div class="info">Centro de Custo:<span class="label">71001 - Administrativo</span></div>
+        <div class="info">Numero Kits:<span class="label"></span></div>
+    </div>
+    <div class="card">
+        <div class="info">Ticket:<span class="label"></span></div>
+        <div class="info">Candidato:<span class="label">Caio Almeida</span></div>
+        <div class="info">CPF:<span class="label">047.845.731-60</span></div>
+        <div class="info">Superior Imediato:<span class="label">Késsya de Lima Rosolini</span></div>
+        <div class="info">Cargo de Admissão:<span class="label">Analista de Pessoas & Cultura Jr</span></div>
+        <div class="info">Substituição:<span class="label">Laura Barros Ribeiro</span></div>
+        <div class="info">E-mail:<span class="label">caio.almeida@amorsaude.com</span></div>
+        <div class="info">User AD:<span class="label">caioalmeida</span></div>
+        <div class="info">Empresa:<span class="label"></span>AmorSaude</div>
+        <div class="info">Centro de Custo:<span class="label">13006 - Pessoas & Cultura</span></div>
+        <div class="info">Numero Kits:<span class="label"></span></div>
+    </div>
+    <div class="card">
+        <div class="info">Ticket:<span class="label"></span></div>
+        <div class="info">Candidato:<span class="label">Júlia Bernardino Marin</span></div>
+        <div class="info">CPF:<span class="label">514.357.068-97</span></div>
+        <div class="info">Superior Imediato:<span class="label"></span></div>
+        <div class="info">Cargo de Admissão:<span class="label">Analista Financeiro PL</span></div>
+        <div class="info">Departamento:<span class="label">Financeiro </span></div>
+        <div class="info">Substituição:<span class="label"></span></div>
+        <div class="info">E-mail:<span class="label"></span></div>
+        <div class="info">User AD:<span class="label"></span></div>
+        <div class="info">Empresa:<span class="label"></span>AmorSaude</div>
+        <div class="info">Centro de Custo:<span class="label">13001 - Financeiro</span></div>
+        <div class="info">Numero Kits:<span class="label"></span></div>
     </div>
 
 </div>
@@ -36,4 +138,5 @@
 <footer>
     <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
 </footer>
-</body></html>
+</body>
+</html>
