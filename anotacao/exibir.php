ab7b4c46e42511443b43f6179dd72c8ccba204f4
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-05-29 12:20:21 -0300

    ♻️ (exibir.php): refatora a inclusão de cabeçalho e metadados HTML
    Centraliza a inclusão de metadados e scripts comuns em um arquivo
    separado, `head.html`, para facilitar a manutenção e garantir
    consistência em todas as páginas. Remove duplicações de código e
    simplifica a estrutura do HTML. Isso também melhora a organização do
    código, tornando-o mais limpo e fácil de gerenciar.

--- anotacao/exibir.php
+++ anotacao/exibir.php
@@ -1,23 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8" />
-    <title>Anotações</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Marcelo de Araujo Almeida">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Adicionar</title>
+    <?php include '../includes/head.html'; ?>
     <link rel="stylesheet" href="../css/exibir.css">
-    <link rel="stylesheet" href="../css/menu-drop.css">
-    <link rel="icon" href="../imagem/favicon/favicon-16x16.png">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="../scripts/menudrop.js" defer></script>
 </head>
 <body>
 <?php include '../includes/header.php'; ?>
 <div class="container">
-<h2>Anotações</h2>
 <div id="lista"></div>
-
 <script>
     fetch('../data/data.json')
         .then(r => r.json())
@@ -40,7 +31,6 @@
                 lista.appendChild(div);
             });
         });
-
     function resolver(id) {
         fetch("resolver.php", {
             method: "POST",
